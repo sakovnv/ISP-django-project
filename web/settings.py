85e25e4b18d6b8bcce27c684f3f2b0331af3ffb6
sakovnv <sakovnv@gmail.com>
2022-05-18 21:21:14 +0300

    set database to mysql

--- web/settings.py
+++ web/settings.py
@@ -66,8 +66,10 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': BASE_DIR.as_posix() + '/' + 'my.cnf',
+        },
     }
 }
 
